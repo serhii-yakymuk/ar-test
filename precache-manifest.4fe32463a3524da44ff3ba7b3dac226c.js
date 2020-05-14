0f5b520084acc52993c35b83038599d59a8ad5cb
Serhii Yakymuk <sergiy.yakymuk@gmail.com>
2020-05-14 15:38:47 +0300

    Updates

---
+++ precache-manifest.4fe32463a3524da44ff3ba7b3dac226c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960a297bbed17daa44cd3d27ed77e282",
+    "url": "/ar-test/index.html"
+  },
+  {
+    "revision": "256269718010a4267b47",
+    "url": "/ar-test/static/css/main.e3fdb249.chunk.css"
+  },
+  {
+    "revision": "d185f5271b1b977cec9f",
+    "url": "/ar-test/static/js/2.e1ebb36c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ar-test/static/js/2.e1ebb36c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "256269718010a4267b47",
+    "url": "/ar-test/static/js/main.878c722e.chunk.js"
+  },
+  {
+    "revision": "1975108c389eda509ac5",
+    "url": "/ar-test/static/js/runtime-main.8dee7142.js"
+  }
+]);
